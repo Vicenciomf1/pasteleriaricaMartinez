9837341651f12bd0423503ff9dce5839885ea7b8
Vicenciomf1 <101673305+Vicenciomf1@users.noreply.github.com>
2022-10-11 06:30:08 +0000

    Protegida la info del acceso a firebase (protegida la API Key, guardada en un baúl de secretos).

--- src/services/Firestore.js
+++ src/services/Firestore.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore, collection, getDocs, query, where, getDoc, doc} from "firebase/firestore";
+import { getFirestore, collection, getDocs, query, where, getDoc, doc } from "firebase/firestore";
 
 // Config de la web app de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDFxFdQQLGGau91jvRlxq1dWk09c1N9MMw",
+  apiKey: process.env['API_KEY_FIREBASE'],
   authDomain: "proyectoreactcoderhouse-9c5ef.firebaseapp.com",
   projectId: "proyectoreactcoderhouse-9c5ef",
   storageBucket: "proyectoreactcoderhouse-9c5ef.appspot.com",
   messagingSenderId: "496924853098",
-  appId: "1:496924853098:web:9db3765e785f72f5405b13"
+  appId: process.env['ID_APP']
 };
 
 // Instanciamos la app y la base de datos de Firebase
@@ -18,13 +18,13 @@ const database = getFirestore(app);
 // Closures para mejorar escalabilidad:
 // Antes era un database.collection().get() y al .then() de eso lo mapeábamos
 const generarTraerDocumentos = (coleccion) => {
-  return async function(){
-    try{
+  return async function() {
+    try {
       const coleccionRef = collection(database, coleccion);  // Referencia de la colección
       const capturaConsulta = await getDocs(coleccionRef);  // Objeto como respuesta a resolver la primera promesa, también es llamado query snapshot (captura/instantánea/radiografía de la consulta)
       const documentos = capturaConsulta.docs;  // Array de documentos
 
-      return documentos.map( documento => ({...documento.data(), id: documento.id}) )  // Mapeamos el array para que nos den la data y el ID de tal documento
+      return documentos.map(documento => ({ ...documento.data(), id: documento.id }))  // Mapeamos el array para que nos den la data y el ID de tal documento
     } catch (error) {
       console.log("Nos hemos encontrado con el siguiente error:", error);  // ¿Cómo evito exponer los errores, y en vez de eso guardarlos en algún log?
     }
@@ -33,8 +33,8 @@ const generarTraerDocumentos = (coleccion) => {
 
 // Antes era un database.collection().where().get() y al .then() de eso lo mapeábamos
 const generarTraerDocumentosConFiltro = (coleccion, campo, operador) => {
-  return async function(valor){
-    try{
+  return async function(valor) {
+    try {
       const coleccionRef = collection(database, coleccion);  // Referencia de la colección
       const consulta = query(coleccionRef, where(campo, operador, valor));  // La única diferencia con el Read de todos los objetos es que acá filtramos con un WHERE, como la query de DQL con Where en SQL.
       const capturaConsulta = await getDocs(consulta);  // Objeto como respuesta a resolver la primera promesa
@@ -44,7 +44,7 @@ const generarTraerDocumentosConFiltro = (coleccion, campo, operador) => {
       }
 
       const documentos = capturaConsulta.docs;  // Array de documentos
-      return documentos.map( documento => ({...documento.data(), id: documento.id}) )  // Mapeamos el array para que nos den la data y el ID de tal documento
+      return documentos.map(documento => ({ ...documento.data(), id: documento.id }))  // Mapeamos el array para que nos den la data y el ID de tal documento
     } catch (error) {
       console.log("Nos hemos encontrado con el siguiente error:", error);
     }
@@ -52,11 +52,11 @@ const generarTraerDocumentosConFiltro = (coleccion, campo, operador) => {
 }
 
 const generarTraerUnDocumento = (coleccion) => {
-  return async function(id){
-    try{
+  return async function(id) {
+    try {
       const documentoRef = doc(database, coleccion, id);  // Referencia del documento
       const documento = await getDoc(documentoRef); // En realidad es la captura del documento, pero como es un solo documento, no es necesario mapearlo y me facilita ponerle ese nombre
-      return {...documento.data(), id: documento.id}
+      return { ...documento.data(), id: documento.id }
     } catch (error) {
       console.log("Nos hemos encontrado con el siguiente error:", error);
     }
